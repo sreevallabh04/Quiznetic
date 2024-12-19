77cc9775c8a0fab02eb205e1082173dfba7f01a9
Sahith-Rao <sahithraolingampally@gmail.com>
2024-12-19 17:46:57 +0530

    login functionality is added

--- backend/Controllers/AuthContoller.js
+++ backend/Controllers/AuthContoller.js
@@ -29,6 +29,44 @@ const signup = async(req,res) => {
     }
 }
 
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await UserModel.findOne({ email });
+        const errorMsg = 'Auth failed email or password is wrong';
+        if (!user) {
+            return res.status(403)
+                .json({ message: errorMsg, success: false });
+        }
+        const isPassEqual = await bcrypt.compare(password, user.password);
+        if (!isPassEqual) {
+            return res.status(403)
+                .json({ message: errorMsg, success: false });
+        }
+        const jwtToken = jwt.sign(
+            { email: user.email, _id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: '24h' }
+        )
+
+        res.status(200)
+            .json({
+                message: "Login Success",
+                success: true,
+                jwtToken,
+                email,
+                name: user.name
+            })
+    } catch (err) {
+        res.status(500)
+            .json({
+                message: "Internal server errror",
+                success: false
+            })
+    }
+}
+
 module.exports= {
-    signup
+    signup,
+    login
 }
--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,10 +1,9 @@
-const {signup} = require('../Controllers/AuthContoller');
-const { signupValidation } = require('../Midllewares/AuthValidation');
+const {signup, login} = require('../Controllers/AuthContoller');
+const { signupValidation, loginValidation } = require('../Midllewares/AuthValidation');
 const router = require('express').Router();
 
-router.post('/login', (req,res) => {
-    res.send('login success');
-});
+
+router.post('/login', loginValidation, login);
 router.post('/signup', signupValidation, signup);
 
 module.exports = router;
