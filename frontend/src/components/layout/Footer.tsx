63a78b532460e5a4400aff1cdfdb8fd8385d2988
sreevallabh <srivallabhkakarala@gmail.com>
2025-03-28 09:06:47 +0530

    Added footer

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="w-full py-3 text-center text-white/80 text-sm relative z-10">
+      <div className="container mx-auto">
+        made by team SSH
+      </div>
+    </footer>
+  );
+};
